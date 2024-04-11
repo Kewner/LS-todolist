72c1dcdcb3e3c8f8bf11661f2be55736020ae9a4
coen <22282057+Kewner@users.noreply.github.com>
2024-04-11 13:05:51 +0200

    Delete all test code

--- todolist.js
+++ todolist.js
@@ -35,12 +35,6 @@ class TodoList {
     return this.todos[index];
   }
 
-  _validateIndex(index) {
-    if (!(index in this.todos)) {
-      throw new ReferenceError(`invalid index: ${index}`);
-    }
-  }
-
   markDoneAt(index) {
     this.itemAt(index).markDone();
   }
@@ -71,84 +65,10 @@ class TodoList {
     let list = this.todos.map(todo => todo.toString()).join('\n');
     return `${title}\n${list}`;
   }
+
+  _validateIndex(index) {
+    if (!(index in this.todos)) {
+      throw new ReferenceError(`invalid index: ${index}`);
+    }
+  }
 }
-
-// test todolist creation
-let list = new TodoList("Today's Todos");
-console.log(list);
-
-// test todo creation
-let todo1 = new Todo("Buy milk");
-let todo2 = new Todo("Clean room");
-let todo3 = new Todo("Go to the gym");
-let todo4 = new Todo("Go shopping");
-
-// test add method
-list.add(todo1);
-list.add(todo2);
-list.add(todo3);
-list.add(todo4);
-console.log(list);
-
-// test size method
-console.log(list.size());  // 4
-
-// test first and last methods
-console.log(list.first());
-console.log(list.last());
-
-let emptyList = new TodoList("Empty List");
-console.log(emptyList.first());
-console.log(emptyList.last());
-
-// test indexAt
-console.log(list.itemAt(1));
-
-// test markDoneAt and markUndoneAt
-list.markDoneAt(1);
-console.log(list);
-
-list.markUndoneAt(1);
-console.log(list);
-
-// test isDone
-console.log(list.isDone()); // false
-
-list.markDoneAt(0);
-list.markDoneAt(1);
-list.markDoneAt(2);
-list.markDoneAt(3);
-console.log(list.isDone()); // true
-
-list.markUndoneAt(2);
-console.log(list.isDone()); // false
-
-// test shift and pop
-console.log(list.shift());
-console.log(list.pop());
-console.log(list);
-
-console.log(emptyList.shift());
-console.log(emptyList.pop());
-console.log(emptyList);
-
-// test removeAt
-let todo5 = new Todo("Feed the cats");
-let todo6 = new Todo("Study for Launch School");
-list.add(todo5);
-list.add(todo6);
-console.log(list);
-
-console.log(list.removeAt(2));
-console.log(list.removeAt(0));
-console.log(list.removeAt(1));
-console.log(list);
-
-// test toString
-list.add(todo1);
-list.add(todo2);
-list.add(todo4);
-list.add(todo5);
-list.add(todo6);
-console.log(`${list}`); // calls toString
-console.log(String(list));
