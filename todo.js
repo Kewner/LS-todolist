fff42f089c8b2980e3615202ab41ccc1cd683798
coen <22282057+Kewner@users.noreply.github.com>
2024-04-11 11:38:49 +0200

    Change description comment in todo file, create TodoList class in todolist file

--- todo.js
+++ todo.js
@@ -1,9 +1,6 @@
 // Class Todo represents a todo item and associated data: the todo
 // title and a flag that shows whether the todo item is done.
 
-// - The Todo class encapsulates todo items.
-// - The TodoList class maintains the collection of Todo objects.
-
 class Todo {
   static DONE_MARKER = 'X';
   static UNDONE_MARKER = ' ';
---
+++ todolist.js
@@ -0,0 +1,15 @@
+// Class TodoList represents a collection of Todo objects.
+// We can perform typical collection-oriented actions on a
+// TodoList object, including iteration and selection.
+
+class TodoList {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  // rest of class needs implementation
+}
+
+let list = new TodoList("Today's Todos");
+console.log(list);
