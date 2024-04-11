cb0928ac3443d9147083818cbe2e4ec2846fe7da
coen <22282057+Kewner@users.noreply.github.com>
2024-04-11 11:51:36 +0200

    Implement add() method

--- todolist.js
+++ todolist.js
@@ -11,9 +11,24 @@ class TodoList {
   }
 
   add(todo) {
+    if (!(todo instanceof Todo)) {
+      throw new TypeError('can only add Todo objects');
+    }
 
+    if (!this.todos.includes(todo)) this.todos.push(todo);
   }
 }
 
 let list = new TodoList("Today's Todos");
+console.log(list);
+
+let todo1 = new Todo("Buy milk");
+let todo2 = new Todo("Clean room");
+let todo3 = new Todo("Go to the gym");
+let todo4 = new Todo("Go shopping");
+
+list.add(todo1);
+list.add(todo2);
+list.add(todo3);
+list.add(todo4);
 console.log(list);
