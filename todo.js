47f22584389f5b16101e84314572f80ac9911841
coen <22282057+Kewner@users.noreply.github.com>
2024-04-11 11:02:34 +0200

    Create Todo class, including first methods

---
+++ todo.js
@@ -0,0 +1,36 @@
+// Class Todo represents a todo item and associated data: the todo
+// title and a flag that shows whether the todo item is done.
+
+// - The Todo class encapsulates todo items.
+// - The TodoList class maintains the collection of Todo objects.
+
+class Todo {
+  static DONE_MARKER = 'X';
+  static UNDONE_MARKER = ' ';
+
+  constructor(title) {
+    this.title = title;
+    this.done = false;
+  }
+
+  toString() {
+    let marker = this.isDone() ? Todo.DONE_MARKER : Todo.UNDONE_MARKER;
+    return `[${marker}] ${this.title}`;
+  }
+
+  markDone() {
+    this.done = true;
+  }
+
+  markUndone() {
+    this.done = false;
+  }
+
+  isDone() {
+    return this.done;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+}
